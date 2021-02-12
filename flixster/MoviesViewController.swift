25ed9c7354045b0c084acdafd9fa0458143e3bad
Okwuolisa Umeokolo <umeokol1@msu.edu>
2021-02-12 11:08:02 -0500

    Imported Alamofire

--- flixster/MoviesViewController.swift
+++ flixster/MoviesViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -59,6 +60,12 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        
+        
         
         return cell
     }

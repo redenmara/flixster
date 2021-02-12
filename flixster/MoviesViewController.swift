ee2c40ddb272049b5f91a8e2a70ea18af3cc7f3e
Okwuolisa Umeokolo <umeokol1@msu.edu>
2021-02-12 10:53:23 -0500

    Added title and synopsis

--- flixster/MoviesViewController.swift
+++ flixster/MoviesViewController.swift
@@ -51,12 +51,14 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
         
         let movie = movies[indexPath.row]
         let title = movie["title"] as! String
+        let synopsis = movie["overview"] as! String
         
-        cell.textLabel?.text = title
+        cell.titleLabel.text = title
+        cell.synopsisLabel.text = synopsis
         
         return cell
     }

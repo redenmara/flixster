9a89549c020cb28bad10775cca6cd0bf5e8f38aa
Okwuolisa Umeokolo <umeokol1@msu.edu>
2021-02-20 16:29:29 -0500

    added empty CollectionView screen

--- flixster/MoviesViewController.swift
+++ flixster/MoviesViewController.swift
@@ -70,7 +70,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         return cell
     }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -78,6 +78,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }

fa8e3b4f4d1e5652aa2a27fe5d3783aa71164c9e
Okwuolisa Umeokolo <umeokol1@msu.edu>
2021-02-20 19:42:34 -0500

    completed superhero grid screen

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Okwuolisa Umeokolo on 2/20/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
